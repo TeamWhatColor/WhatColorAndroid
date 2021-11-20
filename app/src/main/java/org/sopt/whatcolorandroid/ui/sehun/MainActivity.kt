1cb759e24f262ea15927b0464c93145dbb9099bc
s9hn <tnghqn@gmail.com>
2021-11-21 03:57:14 +0900

    [ui]수정한내용

--- app/src/main/java/org/sopt/whatcolorandroid/ui/sehun/MainActivity.kt
+++ app/src/main/java/org/sopt/whatcolorandroid/ui/sehun/MainActivity.kt
@@ -1,13 +1,24 @@
 package org.sopt.whatcolorandroid.ui.sehun
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.whatcolorandroid.R
+import org.sopt.whatcolorandroid.databinding.ActivityMainBinding
+import org.sopt.whatcolorandroid.ui.jeongwon.MissionActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val intent = Intent(this, MissionActivity::class.java) // 확인 버튼 시 미션뷰로이동
+
+        binding.btnCheck.setOnClickListener{
+            startActivity(intent)
+        }
+        setContentView(binding.root)
     }
 }
