38d3735de3662ca06515ccb2b652415f09c4c244
s9hn <tnghqn@gmail.com>
2021-11-21 06:35:14 +0900

    [server]서버

--- app/src/main/java/org/sopt/whatcolorandroid/ui/sehun/MainActivity.kt
+++ app/src/main/java/org/sopt/whatcolorandroid/ui/sehun/MainActivity.kt
@@ -3,14 +3,20 @@ package org.sopt.whatcolorandroid.ui.sehun
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.content.res.ResourcesCompat
 import org.sopt.whatcolorandroid.R
 import org.sopt.whatcolorandroid.api.ServiceCreator
 import org.sopt.whatcolorandroid.databinding.ActivityMainBinding
 import org.sopt.whatcolorandroid.ui.jeongwon.MissionActivity
+import org.sopt.whatcolorandroid.util.MyApplication
+import org.sopt.whatcolorandroid.util.enqueueUtil
 import retrofit2.Call
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val sharedPreferences = MyApplication.mySharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,10 +27,37 @@ class MainActivity : AppCompatActivity() {
         binding.btnCheck.setOnClickListener{
             startActivity(intent)
         }
+
         setContentView(binding.root)
+
+        initnetwork()
+    }
+
+    fun onStart(){
+        super.onStart()
+
     }
 
     private fun initnetwork(){
-        val call: Call<ResponseColorData> = ServiceCreator.
+        val uuid = sharedPreferences.getValue("uuid","")
+        Log.d("hihi", uuid)
+
+        val call = ServiceCreator.apiService.getMain(uuid)
+
+        call.enqueueUtil(
+            onSuccess = {
+                val color = it.data.color
+                binding.tvName.text= it.data.nickname
+                binding.tvTeam.text = it.data.color+" TEAM"
+                if (color == "blue") {
+                    binding.ivColor.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.icon_blue_small, null))
+                } else if (color == "pink") {
+                    binding.ivColor.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.icon_pink_small, null))
+                } else if (color == "yellow") {
+                    binding.ivColor.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.icon_yellow_small, null))
+                }
+
+            }
+        )
     }
 }
--- app/src/main/java/org/sopt/whatcolorandroid/ui/sehun/ResponseColorData.kt
+++ app/src/main/java/org/sopt/whatcolorandroid/ui/sehun/ResponseColorData.kt
@@ -1,13 +1,13 @@
 package org.sopt.whatcolorandroid.ui.sehun
 
 data class ResponseColorData(
-    private val status: Int,
-    private val success: Boolean,
-    private val data: Data
+    val status: Int,
+    val success: Boolean,
+    val data: Data
 ) {
     data class Data(
-        private val nickname: String,
-        private val color: String,
-        private val content : String
+        val nickname: String,
+        val color: String,
+        val content : String
     )
 }
